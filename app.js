27f352b0915f74d40a71e31bcf5b51d9bd5ff0bb
jayrnoel <8006157+jayrnoel@users.noreply.github.com>
2022-09-06 21:55:07 +0800

    completed the design
    Day 2 of Easybank website.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const menu = document.querySelector(".nav__hamburger");
 const menuImg = document.querySelector(".nav__hamburger__img");
+const links = document.querySelectorAll(".nav__link");
 const overlay = document.querySelector(".overlay");
 const navMenu = document.querySelector(".nav__menu");
 
 menu.addEventListener("click", () => {
   navMenu.classList.toggle("open");
+  toggleMenu();
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+    toggleMenu();
+  });
+});
+
+function toggleMenu() {
   if (navMenu.classList.contains("open")) {
     overlay.classList.add("open");
     menuImg.src = "./images/icon-close.svg";
@@ -14,4 +26,4 @@ menu.addEventListener("click", () => {
     menuImg.src = "./images/icon-hamburger.svg";
     menuImg.classList.remove("close");
   }
-});
+}
